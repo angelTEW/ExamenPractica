5653deb48e3c6d056349382a90db25954dcf4921
Angel <angelg.ramirez@iteso.mx>
2026-09-21 12:36:01 -0600

    Mis cambios locales

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ const express = require('express'); // O un script simple de Node
 const fs = require('fs');
 
 function calcularPromedio(notas) {
-  let suma = 0;
-  for (let i = 0; i < notas.length; i++) {
-    suma += notas[i];
-  }
-  return suma / notas.length;
+  const notasValidas = notas.filter(nota => nota >= 60);
+  const descartadas = notas.length - notasValidas.length;
+  
+  console.log(`Notas descartadas: ${descartadas}`);
+
+  if (notasValidas.length === 0) return 0;
+
+  const suma = notasValidas.reduce((acc, curr) => acc + curr, 0);
+  return suma / notasValidas.length;
 }
 
 const calificaciones = [85, 90, 78, 92, 88];
